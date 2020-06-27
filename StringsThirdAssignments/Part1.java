c9d5debc8b05922f2f94b5342b58c23b80069d45
Tara Patel <t.jeanne.patel@gmail.com>
2020-06-27 16:11:27 -0500

    finished third Strings assignment

--- StringsThirdAssignments/Part1.java
+++ StringsThirdAssignments/Part1.java
@@ -9,6 +9,8 @@ import edu.duke.*;
 
 public class Part1 {
     int findStopCodon(String dna, int startIndex, String stopCodon) {
+        dna = dna.toUpperCase();
+        stopCodon = stopCodon.toUpperCase();
         int codonIndex = -1;
         int tempStart = startIndex;
         while (codonIndex == -1) {
@@ -27,6 +29,7 @@ public class Part1 {
     }
     
     String findGene(String dna) {
+        dna = dna.toUpperCase();
         int startIndex = dna.indexOf("ATG");
         if (startIndex == -1) {
             return "";
@@ -45,6 +48,7 @@ public class Part1 {
     }
     
     void printAllGenes(String dna) {
+        dna = dna.toUpperCase();
         while(true) {
             String gene = findGene(dna);
             if (gene.isEmpty()) {
@@ -57,6 +61,7 @@ public class Part1 {
     }
     
     StorageResource getAllGenes(String dna) {
+        dna = dna.toUpperCase();
         StorageResource genes = new StorageResource();
         while(true) {
             String gene = findGene(dna);
@@ -82,18 +87,48 @@ public class Part1 {
     }
     
     double cgRatio(String dna) {
-        dna = dna.toLowerCase();
+        dna = dna.toUpperCase();
         double cCount = 0.0;
         double gCount = 0.0;
         for (char c: dna.toCharArray()) {
-            if (c == 'c') {
+            if (c == 'C') {
                 cCount ++;
             }
-            else if (c == 'g') {
+            else if (c == 'G') {
                 gCount ++;
             }
         }
         return (cCount + gCount) / dna.length();
+    }
+    
+    void processGenes(StorageResource sr) {
+        int overNineCount = 0;
+        int cgRatioCount = 0;
+        int longestLength = 0;
+        for (String gene: sr.data()) {
+            if(gene.length() > 60) {
+                System.out.println("A gene over 60 characters! " + gene);
+                overNineCount ++;
+            }
+            if(cgRatio(gene) > .35) {
+                System.out.println("This one has a lot of c's and g's! " + gene);
+                cgRatioCount ++;
+            }
+            if (gene.length() > longestLength) {
+                longestLength = gene.length();
+            }
+        }
+        System.out.println("There were " + sr.size() + " genes in this DNA strand.");
+        System.out.println("There were " + overNineCount + " genes over 60 chars.");
+        System.out.println("There were " + cgRatioCount + " genes with a cg ratio > .35");
+        System.out.println("The longest gene was " + longestLength + ".");
+    }
+    
+    void testProcessGenes() {
+        FileResource fr = new FileResource("brca1line.fa");
+        String dna = fr.asString();
+        StorageResource testGenes = getAllGenes(dna.toUpperCase());
+        processGenes(testGenes);
     }
     
     void testGetAllGenes() {
